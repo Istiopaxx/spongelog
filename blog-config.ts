f1335c7733cbf6e7fd5ed0a3bc35ecf3a0c9eac5
KeisLuv5991 <chmhj9997@gmail.com>
2023-02-10 16:02:27 +0900

    change blog setting

--- blog-config.ts
+++ blog-config.ts
@@ -1,22 +1,23 @@
 const config = {
-  title: 'KeisLuvlog',     // Blog name
-  author: 'KeisLuv5991.', // Your name
-  description: 'What is Real?  How do you define Real?  I`ve been lived in the dream world..',
-  siteUrl: 'https://keisluvlog.netlify.app',
-  thumbnail: '/thumbnails/main.png',  // Default blog thumbnail. i.e. '/thumbnails/main.png'
+  title: 'SpongeLog', // Blog name
+  author: 'Istiopaxx', // Your name
+  description:
+    'What is Real?  How do you define Real?  I`ve been lived in the dream world..',
+  siteUrl: 'https://spongelog.netlify.app',
+  thumbnail: '/thumbnails/main.png', // Default blog thumbnail. i.e. '/thumbnails/main.png'
   social: {
-    github: 'keisluv5991',   // GitHub username
-    twitter: '',        // Twitter username
-    facebook: '',       // Facebook username
-    linkedin: '',       // LinkedIn username
-    instagram: '',      // Instagram username
+    github: 'istiopaxx', // GitHub username
+    twitter: '', // Twitter username
+    facebook: '', // Facebook username
+    linkedin: '', // LinkedIn username
+    instagram: '', // Instagram username
   },
   comment: {
-    utterances: 'keisluv5991/keisluvlog', // Your GitHub repository
-    disqusShortName: '',  // Disqus short name
+    utterances: 'istiopaxx/spongelog', // Your GitHub repository
+    disqusShortName: '', // Disqus short name
   },
   icon: 'src/images/favicon.png', // path of blog icon
-  googleAnalytics: 'G-DY6PQQ104H',  // Google Analytics tracking id
+  googleAnalytics: 'G-DY6PQQ104H', // Google Analytics tracking id
 };
 
 export default config;
